1a28ccd0ef88dbec2e4f3fe1de9eccbbe3d3782d
Himanshu <himanshughorai000@gmail.com>
2025-03-03 23:28:09 +0530

    i add some js but is not done

---
+++ script.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll('#todo_input');
+const list = document.querySelector('#todo_items');
+
+todo_input.addEventListener(
+    "keypress",
+    function(event){
+
+        console.log(event)
+    }
+);
+
